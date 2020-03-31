405c51f5148746c4e7497870958e889cf6fbfbb5
Juan dela Cruz <37132148+hansvomkreuz@users.noreply.github.com>
2020-03-31 16:35:33 +1100

    Updates

--- R/fovealCentre.R
+++ R/fovealCentre.R
@@ -2,13 +2,13 @@
 #' Merging fovealCentre and fovealCentreRegional functions into one
 #' The 'regional' function is a limited version of the original 'fovealCentre' function
 #' This function looks at minimum value within a section of the eye:
-#' a. 6th to 20th horizontal layers
+#' a. 8th to 18th horizontal layers
 #' b. 157th to 356th vertical layers
 fovealCentre <- function(consData,regionalEyeCenter = TRUE){
     if(regionalEyeCenter){
         data <- consData %>% 
             mutate(HL = dense_rank(HL)) %>% 
-            filter(between(HL,6,20)) %>% 
+            filter(between(HL,8,18)) %>% 
             select(c(1:7,164:363))
     } else {
         data <- consData
@@ -17,7 +17,7 @@ fovealCentre <- function(consData,regionalEyeCenter = TRUE){
         select(-c(1:5))
     coordinate <- which(data == min(data, na.rm = TRUE), arr.ind = TRUE)
     if(regionalEyeCenter){
-        coordinate[,1] <- coordinate[,1] + 5
+        coordinate[,1] <- coordinate[,1] + 7
         coordinate[,2] <- coordinate[,2] + 156
     }
     return(coordinate)
